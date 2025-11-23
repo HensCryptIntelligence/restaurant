52e7872a6be64d770d6cf60c7952169e610e8a42
harv-y <harvimandagi@gmail.com>
2025-11-24 00:42:55 +0700

    KLOT

--- features/auth/controllers/logout.php
+++ features/auth/controllers/logout.php
@@ -1,11 +1,13 @@
 <?php
 session_start();
-
-// Hapus semua session
 session_unset();
 session_destroy();
 
-// Redirect ke halaman login
+// Anti back
+header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
+
 header("Location: ../views/login.php");
 exit;
 ?>
